7f57efe455854eb87459dffa90f4c0fcc03a9f22
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-08-22 16:50:01 +0100

    add redux

---
+++ src/redux/rockets/rockets.js
@@ -0,0 +1,5 @@
+const ADD_ROCKET = 'space-hub/rockets/ADD_ROCKET';
+const RESERVE = 'space-hub/rockets/RESERVE';
+const CANCEL_RESERVE = 'space-hub/rockets/CANCEL_RESERVE';
+
+const initialState = [];
