5f3580dc52aa47a36c9b621fbe1c5aaccaa507c6
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-08-22 16:58:45 +0100

    add redux and fixed error

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,13 +1,13 @@
-import { configureStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import missions from './missions/missions';
-import rocketsReducer from './rockets/rockets';
+// import { configureStore, applyMiddleware, combineReducers } from 'redux';
+// import thunk from 'redux-thunk';
+// import missions from './missions/missions';
+// import rocketsReducer from './rockets/rockets';
 
-const reducer = combineReducers({
-  missions,
-  rocketsReducer,
-});
+// const reducer = combineReducers({
+//   missions,
+//   rocketsReducer,
+// });
 
-const store = configureStore(reducer, applyMiddleware(thunk));
+// const store = configureStore(reducer, applyMiddleware(thunk));
 
-export default store;
+// export default store;
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,5 +1,5 @@
-const ADD_ROCKET = 'space-hub/rockets/ADD_ROCKET';
-const RESERVE = 'space-hub/rockets/RESERVE';
-const CANCEL_RESERVE = 'space-hub/rockets/CANCEL_RESERVE';
+// const ADD_ROCKET = 'space-hub/rockets/ADD_ROCKET';
+// const RESERVE = 'space-hub/rockets/RESERVE';
+// const CANCEL_RESERVE = 'space-hub/rockets/CANCEL_RESERVE';
 
-const initialState = [];
+// const initialState = [];
