94403192bb275cdfb0baf8e2d0c495dcfb93bffc
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-08-22 22:26:15 +0100

    render the data to the ui

--- src/Components/Mission.js
+++ src/Components/Mission.js
@@ -1,44 +1,44 @@
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { getMissions } from '../redux/missions/missions';
-// import MissionItem from './MissionItem';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMissions } from '../redux/missions/missions';
+import MissionItem from './MissionItem';
 
-// const Mission = () => {
-//   const status = useSelector((state) => state.missions.status);
-//   const missions = useSelector((state) => state.missions.data);
-//   const dispatch = useDispatch();
+const Mission = () => {
+  const status = useSelector((state) => state.missions.status);
+  const missions = useSelector((state) => state.missions.data);
+  const dispatch = useDispatch();
 
-//   useEffect(() => {
-//     if (status === '') {
-//       dispatch(getMissions());
-//     }
-//   }, [status, dispatch]);
+  useEffect(() => {
+    if (status === '') {
+      dispatch(getMissions());
+    }
+  }, [status, dispatch]);
 
-//   return (
-//     <section data-testid="missions-section" className="missions">
-//       <table>
-//         <thead>
-//           <tr>
-//             <th> Mission </th>
-//             <th> Description </th>
-//             <th> Status </th>
-//             <th> </th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {missions.map((mission) => (
-//             <MissionItem
-//               key={mission.id}
-//               name={mission.name}
-//               description={mission.description}
-//               id={mission.id}
-//               reserved={mission.reserved}
-//             />
-//           ))}
-//         </tbody>
-//       </table>
-//     </section>
-//   );
-// };
+  return (
+    <section data-testid="missions-section" className="missions">
+      <table>
+        <thead>
+          <tr>
+            <th> Mission </th>
+            <th> Description </th>
+            <th> Status </th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {missions.map((mission) => (
+            <MissionItem
+              key={mission.id}
+              name={mission.name}
+              description={mission.description}
+              id={mission.id}
+              reserved={mission.reserved}
+            />
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
 
-// export default Mission;
+export default Mission;
