255803ee3351d5367aace38391367cedf2da64ac
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2022-08-22 22:21:32 +0100

    add the data from the api

--- src/Components/JoinMission.js
+++ src/Components/JoinMission.js
@@ -1,7 +1,26 @@
-import React from 'react';
+// import React from 'react';
+// import { useSelector } from 'react-redux';
 
-function JoinMission() {
-  return <div>JoinMission</div>;
-}
+// const JoinedMissions = () => {
+//   const missions = useSelector((state) => state.missions.data);
+//   const joinedMissions = missions.filter((mission) => mission.reserved);
 
-export default JoinMission;
+//   return (
+//     <div>
+//       <h2> My Missions </h2>
+//       <ul className="missions-container">
+//         {joinedMissions.length > 0 ? (
+//           joinedMissions.map((mission) => (
+//             <li key={mission.id} className="mission">
+//               {mission.name}
+//             </li>
+//           ))
+//         ) : (
+//           <li className="mission"> You haven&apos;t joined any mission</li>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default JoinedMissions;
